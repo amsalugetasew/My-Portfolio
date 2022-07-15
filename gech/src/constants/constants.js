81fbda4b505baf38e1cbd596d5264b0f6c8cef38
unknown <amsalugetasew>
2022-07-15 02:07:53 -0700

    My personal Portfolio

--- gech/src/constants/constants.js
+++ gech/src/constants/constants.js
@@ -1,31 +1,30 @@
+import Website from "../image/gechweb.PNG"
 export const projects = [
   {
     title: "Personal Portfolio",
     description:
       "This is a personal Portfolio project built React, Material UI.",
-    image: "#",
-    // image: "https://i.ytimg.com/vi/UhXZHrXkuKU/maxresdefault.jpg",
+    image: "https://doc-00-6o-docs.googleusercontent.com/docs/securesc/9v4746q04aek1r3gif4pd6ktb1rtg6td/2400h7r4c23cl3qbf2u5b6rjkdpp8smt/1657872525000/10905391362498925260/10905391362498925260/1hrWaHl02kv9VcdtQ9iOspPZbGGWqdq1A?ax=ACxEAsYrxRAsyxz3kqC0W_4Xd7xK-qj4bcVuF6yArtn0b6WNwl_eIlfMIl5CZzYddsq1B8sJqs8qX6q2K8ICI_HPUExo4Iu1uLiKVvHXfA3K3ehrVcndcjmjdx9QFuk_q4qRtG5JZwvMMs7Z5kkv9UOfJPnlyUcnHvsS7CYtq_NIrLmWslzMQ9MrpuxvNjMDydid7ubMiLQAveSaeFVCu06EpV6zviHlUnR9PlnMSiusXAjlqaJtO2ft2KlduWfnXClHPif0cI5FfxRig1Q4ryefFpIcEYxF2usv-UZLXQ626SIjvln8j4DLmIygQEhRL3d6XOpR3ufWAf_GujCdUBsJ1EGHfgK1xHtzY2AMH3h-ca5miu_-r9dA7h3Ze7NrvkAboYly4ULSZF5Bf-JvG4w84RQG835htaMO6ouqqBUrCkIE8TceTDqbEfph0CUqiL4SBDW-HiGlwSRPFkyHJ3z05o0zyIbuUBgworwGrdexMLgOBp0sLvIdCgFdW6Y_HDzKhlORLcEuQeVkVaby4dAuu_OcZoGgtfdx95SUEkRvIWnM-0y_2c9i_pYItLz07fUqXS74xRgjaeDbja9LFKJC0T_WOct86Ol67jLElTHdAGZo1Ne_P3I7miB8AJXTaH1ygTeLVcchZ6UhlzfLGJ-X330zzCf3HWMK1slYRfa6f9igDTgI3z4vweo2LksWG4uIND3e05VL2zh6sxNWeK43lNV2KM5zd15eK2Zkgh0M-gVaIedEMUpB8oQ&uuid=d8e065ba-b509-40b0-a1bf-4159c7142669&authuser=0",
     tags: ["React", "Material Design","JSX","Styled-Component"],
-    source: "https://github.com/amsalugetasew/slack-clonee",
+    source: "https://github.com/amsalugetasew/My-Portfolios",
     visit: "https://slack-clone-a49ae.web.app/",
     id: 0,
   },
   {
-    title: "Linkedin Clone",
+    title: "AGTC Website",
     description:
       "This is a linkedin clone project built React, Redux, Firebase, Material UI.",
-    image: "https://i.ytimg.com/vi/NxozFN0tc48/maxresdefault.jpg",
+      image: "https://doc-0c-6o-docs.googleusercontent.com/docs/securesc/9v4746q04aek1r3gif4pd6ktb1rtg6td/eehbfervefffqoglaj1hs9orqf15npgc/1657874175000/10905391362498925260/10905391362498925260/1bjN3T8LWFEqjMwQIv_WR7stZGNcfOiT8?ax=ACxEAsZhAEUJEI5W1s6Wyax2hpkjAqtNUitLQX8zdwVr6yup1_Zm5oudmT8Mh5GoN5-I21bvIiAbzHXzcYe-J38BAlCmvVPV70WsjacB0JEPTmyUEnJQDohOeeKWwn0KCCshPuH6mTD6Wyzlnwd7SBPMCMFn7KK-1g2JIQSCGl1IGJCRpFDZ7z0njoLqAIc5bhlNEIw9oxD4KidMcCcqSZXS4D_8u-nQd0of8I00mm6qM3TofF0mFaFPNeLCxJ39i1TTEDDN2GuJ4gAVBYbCupvh2IDtjjuX-0yiUvcu8gISxxVifAxqH-MfcjB1wKrmBUpb_vC5-WEXn8SVo2XJ0Kq3UWBJlVanCQYEPRwcC3_VuEEAlbnTeCBLgiCiYEXDlF0D_vupt7fcE4HcJuo_adY2xZs0jUeVOyO24fdyziWgfsM5Xlcg3PhnxdfgCNE3lmlK0rxezrG2Pb73RggvRG3c7HmlgIOYDptTJomFiuWqWNDM8h8MdR43GZtmCk_UhV13ZNNyXW5R_Zw5OcgNeG86K0u1pT7g5lfTUF0m2VqDC0wH7uPzd3W5jJK0mS0o9f5B4x14I9p9xuCsBb35bJ1FCInIFx597XZEge64dbwybJfODqXfNVZrHLiQF7SOiRB6fWVXh9qjyKZP3glHxhmJlAahV_v1no5Qfov8LtrhFGpwRHEHEMttZ1Bo4tWJ1WmbpkqxSPkjaHSpP1QvL7IKnHe_ofiCjLTnZwcd3EvoFuDcncU5UtHWtXg&uuid=77f464c8-b7fd-497a-a71c-0c12e92d28ea&authuser=0",
     tags: ["React", "Redux", "Firebase", "Material Design"],
     source: "https://github.com/Teshie/linkedin-clone",
     visit: "https://github.com/Teshie/linkedin-clone",
     id: 2,
   },
   {
-    title: "The NewYork Times Clone",
+    title: "Admin Dashboard",
     description:
       "This is a The NewYork Times Clone project built React, Redux, Firebase, Material UI.",
-    image:
-      "https://s3.amazonaws.com/libapps/accounts/24313/images/TRILOGY%E2%80%94iPhone%E2%80%94iPad%E2%80%94Laptop_V2.jpg",
+      image: "https://doc-00-6o-docs.googleusercontent.com/docs/securesc/9v4746q04aek1r3gif4pd6ktb1rtg6td/2400h7r4c23cl3qbf2u5b6rjkdpp8smt/1657872525000/10905391362498925260/10905391362498925260/1hrWaHl02kv9VcdtQ9iOspPZbGGWqdq1A?ax=ACxEAsYrxRAsyxz3kqC0W_4Xd7xK-qj4bcVuF6yArtn0b6WNwl_eIlfMIl5CZzYddsq1B8sJqs8qX6q2K8ICI_HPUExo4Iu1uLiKVvHXfA3K3ehrVcndcjmjdx9QFuk_q4qRtG5JZwvMMs7Z5kkv9UOfJPnlyUcnHvsS7CYtq_NIrLmWslzMQ9MrpuxvNjMDydid7ubMiLQAveSaeFVCu06EpV6zviHlUnR9PlnMSiusXAjlqaJtO2ft2KlduWfnXClHPif0cI5FfxRig1Q4ryefFpIcEYxF2usv-UZLXQ626SIjvln8j4DLmIygQEhRL3d6XOpR3ufWAf_GujCdUBsJ1EGHfgK1xHtzY2AMH3h-ca5miu_-r9dA7h3Ze7NrvkAboYly4ULSZF5Bf-JvG4w84RQG835htaMO6ouqqBUrCkIE8TceTDqbEfph0CUqiL4SBDW-HiGlwSRPFkyHJ3z05o0zyIbuUBgworwGrdexMLgOBp0sLvIdCgFdW6Y_HDzKhlORLcEuQeVkVaby4dAuu_OcZoGgtfdx95SUEkRvIWnM-0y_2c9i_pYItLz07fUqXS74xRgjaeDbja9LFKJC0T_WOct86Ol67jLElTHdAGZo1Ne_P3I7miB8AJXTaH1ygTeLVcchZ6UhlzfLGJ-X330zzCf3HWMK1slYRfa6f9igDTgI3z4vweo2LksWG4uIND3e05VL2zh6sxNWeK43lNV2KM5zd15eK2Zkgh0M-gVaIedEMUpB8oQ&uuid=d8e065ba-b509-40b0-a1bf-4159c7142669&authuser=0",
     tags: ["React", "Firebase", "Context API"],
     source: "https://github.com/Gracetemitope/Positioning-project",
     visit: "https://gracetemitope.github.io/Positioning-project/",
@@ -35,7 +34,7 @@ export const projects = [
   {
     title: "Tribute Page",
     description: "This is a Tribute page made using HTML5, CSS and JavaScript",
-    image: "https://i.ytimg.com/vi/BtExsVNopCs/maxresdefault.jpg",
+    image: "https://doc-00-6o-docs.googleusercontent.com/docs/securesc/9v4746q04aek1r3gif4pd6ktb1rtg6td/2400h7r4c23cl3qbf2u5b6rjkdpp8smt/1657872525000/10905391362498925260/10905391362498925260/1hrWaHl02kv9VcdtQ9iOspPZbGGWqdq1A?ax=ACxEAsYrxRAsyxz3kqC0W_4Xd7xK-qj4bcVuF6yArtn0b6WNwl_eIlfMIl5CZzYddsq1B8sJqs8qX6q2K8ICI_HPUExo4Iu1uLiKVvHXfA3K3ehrVcndcjmjdx9QFuk_q4qRtG5JZwvMMs7Z5kkv9UOfJPnlyUcnHvsS7CYtq_NIrLmWslzMQ9MrpuxvNjMDydid7ubMiLQAveSaeFVCu06EpV6zviHlUnR9PlnMSiusXAjlqaJtO2ft2KlduWfnXClHPif0cI5FfxRig1Q4ryefFpIcEYxF2usv-UZLXQ626SIjvln8j4DLmIygQEhRL3d6XOpR3ufWAf_GujCdUBsJ1EGHfgK1xHtzY2AMH3h-ca5miu_-r9dA7h3Ze7NrvkAboYly4ULSZF5Bf-JvG4w84RQG835htaMO6ouqqBUrCkIE8TceTDqbEfph0CUqiL4SBDW-HiGlwSRPFkyHJ3z05o0zyIbuUBgworwGrdexMLgOBp0sLvIdCgFdW6Y_HDzKhlORLcEuQeVkVaby4dAuu_OcZoGgtfdx95SUEkRvIWnM-0y_2c9i_pYItLz07fUqXS74xRgjaeDbja9LFKJC0T_WOct86Ol67jLElTHdAGZo1Ne_P3I7miB8AJXTaH1ygTeLVcchZ6UhlzfLGJ-X330zzCf3HWMK1slYRfa6f9igDTgI3z4vweo2LksWG4uIND3e05VL2zh6sxNWeK43lNV2KM5zd15eK2Zkgh0M-gVaIedEMUpB8oQ&uuid=d8e065ba-b509-40b0-a1bf-4159c7142669&authuser=0",
     tags: ["HTML5", "CSS", "JavaScript"],
     source: "https://github.com/Teshie/Tribute-Page",
     visit: "https://teshie.github.io/Tribute-Page/",
