0ff43688fae95a520d6aa14443b6a141a99fa64a
Getasew <42133391+amsalugetasew@users.noreply.github.com>
2022-08-31 07:45:42 -0700

    Update constants.js

--- gech/src/constants/constants.js
+++ gech/src/constants/constants.js
@@ -1,6 +1,7 @@
 import AGTC from "../image/agtc.PNG"
 import MyPortfolio from "../image/gechweb.PNG"
 import MyEcommerce from "../image/My-ecommerce.PNG"
+import MyTax from "../image/tax.PNG"
 export const projects = [
   {
     title: "Personal Portfolio",
@@ -20,7 +21,7 @@ export const projects = [
     tags: ["React", "Redux", "MongoDb", "Material Design"],
     source: "https://github.com/amsalugetasew/AGTC",
     visit: "https://github.com/amsalugetasew/AGTC",
-    id: 2,
+    id: 1,
   },
   {
     title: "GHabesha Ecommerce Website",
@@ -32,7 +33,16 @@ export const projects = [
     visit: "https://ghabesha.netlify.app/",
     id: 2,
   },
-  
+  {
+    title: "E-tax web App",
+    description:
+      "This is a E-Tax Web App project built React, Tailwind,  SASS.",
+      image: MyTax,
+    tags: ["React", "Tailwind",  "SASS"],
+    source: "https://github.com/amsalugetasew/Bereket_Tax",
+    visit: "https://tax-test.netlify.app/",
+    id: 3,
+  },
 
 
 ];
